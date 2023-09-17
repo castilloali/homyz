645a7ba481ced2121445f5eeba62d7f0c3d2dd45
cass <alicastilo1097@gmail.com>
2023-09-17 22:51:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+
+
+function App() {
+
+
+  return (
+    <>
+      <Button size="large" variant="contained" color="success">
+        Success
+      </Button>
+    </>
+  );
+}
+
+export default App
