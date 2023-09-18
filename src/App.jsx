92452f845635229b46840a6d46756b0150962945
cass <alicastilo1097@gmail.com>
2023-09-18 06:07:17 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Button } from "@mui/material";
 
+import './App.css'
 
 function App() {
 
-
   return (
     <>
-      <Button size="large" variant="contained" color="success">
-        Success
-      </Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+// import { Box, Link } from "@mui/material";
+
+const Header = () => {
+    return (
+      // <div className='test' sx={{padding: "100"}}>
+      //   <h1>Header </h1>
+      //   <Box>
+      //     <Link href="#" underline="always">
+      //       {"www"}
+      //     </Link>
+      //     <Link href="#" underline="always">
+      //       {"www"}
+      //     </Link>
+      //     <Link href="#" underline="always">
+      //       {"www"}
+      //     </Link>
+      //     <Link href="#" underline="always">
+      //       {"www"}
+      //     </Link>
+      //     <Link href="#" underline="always">
+      //       {"www"}
+      //     </Link>
+      //   </Box>
+      // </div>
+      <div className="h-24 bg-blue-500">This is a styled div element.</div>
+    );
+}
+
+export default Header
