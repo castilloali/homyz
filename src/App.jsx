5cffaa84f09e46e9d9842fbd5f6d4800dab64e7e
cass <alicastilo1097@gmail.com>
2023-09-18 06:29:09 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Header from './Components/header';
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <Header />
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-// import { Box, Link } from "@mui/material";
+import { Box, Link } from "@mui/material";
 
 const Header = () => {
     return (
-      // <div className='test' sx={{padding: "100"}}>
-      //   <h1>Header </h1>
-      //   <Box>
-      //     <Link href="#" underline="always">
-      //       {"www"}
-      //     </Link>
-      //     <Link href="#" underline="always">
-      //       {"www"}
-      //     </Link>
-      //     <Link href="#" underline="always">
-      //       {"www"}
-      //     </Link>
-      //     <Link href="#" underline="always">
-      //       {"www"}
-      //     </Link>
-      //     <Link href="#" underline="always">
-      //       {"www"}
-      //     </Link>
-      //   </Box>
-      // </div>
-      <div className="h-24 bg-blue-500">This is a styled div element.</div>
+      <div className=''>
+        <h1>Header </h1>
+        <Box>
+          <Link href="#" underline="always">
+            {"www"}
+          </Link>
+          <Link href="#" underline="always">
+            {"www"}
+          </Link>
+          <Link href="#" underline="always">
+            {"www"}
+          </Link>
+          <Link href="#" underline="always">
+            {"www"}
+          </Link>
+          <Link href="#" underline="always">
+            {"www"}
+          </Link>
+        </Box>
+      </div>
+      // <div className="h-24 text-2xl bg-blue-500">This is a styled div element.</div>
     );
 }
 
